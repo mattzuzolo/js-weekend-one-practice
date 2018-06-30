05a89ea62ab4b578890d90bf7233fbf7fa8dd681
github username <github email address>
2018-06-30 14:01:08 -0400

    small changes

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const theFourRulesForThis= [
 //this function will create elements
 function createList (theFourRulesForThis) {
 
-    //create unorded list
+    //create unordered list element
     let ul = document.createElement("ul");
 
     //iterate over theFourRulesForThis array to create individual list items
     for (let i = 0; i < theFourRulesForThis.length; i++){
       //generate list item at the start of each iteration
       let li = document.createElement("li");
-      //appendchild to list item with new text node containing string from array indicies
+      //add string from array indicies to the new list item
       li.appendChild(document.createTextNode(theFourRulesForThis[i]));
-      //appendchild list item to the list
+      //add list item to the unordered list
       ul.appendChild(li);
     }
-    //return to the list
+    //return the list
     return ul;
 }
 
-//add the unorded list to the document inside the list container div
+//add the unordered list to the document inside the list container div
 document.getElementById("list-container").appendChild(createList(theFourRulesForThis));
